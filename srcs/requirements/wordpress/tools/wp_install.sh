cb0edd0a463f2ae6b077215979fef2bc3d7e81dc
allab <allab@xubuntu.myguest.virtualbox.org>
2024-01-05 19:04:27 +0100

    wp cli works

--- srcs/requirements/wordpress/tools/wp_install.sh
+++ srcs/requirements/wordpress/tools/wp_install.sh
@@ -5,15 +5,16 @@ if [-f ./wordpress ]; then
 else
     wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
-    sudo mv wp-cli.phar /usr/local/bin/wp
+    mv wp-cli.phar /usr/local/bin/wp
 
     wp core download --allow-root
+    sleep 10
     wp config create --allow-root \
         --dbname=$DB_NAME \
         --dbuser=$DB_USER \
         --dbpass=$DB_PASSWORD \
         --dbhost=$DB_HOST
-    wp core install --url="http://alboumed.42.fr" --title="Blog Title" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
-    wp user create $WP_USER $WP_USER_EMAIL --role=subscriber --user_pass=$WP_USER_PASSWORD
+    wp core install --allow-root --url="http://alboumed.42.fr" --title="Blog Title" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+    wp user create --allow-root $WP_USER $WP_USER_EMAIL --role=subscriber --user_pass=$WP_USER_PASSWORD
 fi
 /usr/sbin/php-fpm7.3 -F
